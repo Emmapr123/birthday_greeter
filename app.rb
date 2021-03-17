c1a76f8e252e5f6a98c3d716f5b88a70330669ce
Emmapr123 <emmapriester@hotmail.com>
2021-03-17 00:01:50 +0000

    on my localhost, I can now submit a name, day and month, and it will tell me how many days it iwll be until that persons birthday.

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require './birthday_calculator'
+
+get '/' do
+  erb :index
+end 
+
+post '/birthday' do
+  $Birthday = Birthday.new(params[:name])
+  $Birthday.when_is_my_birthday(params[:month], params[:day])
+end 
+
+# get '/greeting' do
+# end 
+
--- birthday_calculator.rb
+++ birthday_calculator.rb
@@ -1,4 +1,8 @@
-class Birtday
+class Birthday
+
+  def initialize(name)
+    @name = name
+  end
 
   def calculate_birthday(month, day, year = 2021)
     birthday = Time.new(year, month, day)
@@ -9,21 +13,21 @@ class Birtday
     Time.new.strftime('%j')
   end 
 
-  def when_is_my_birthday(name, month, day, year=2021)
+  def when_is_my_birthday(month, day, year=2021)
     @birthday = calculate_birthday(month, day, year=2021)
     @today = calculate_current_day
-    @birthday == @today ?  happy_birthday(name) : count(name)
+    @birthday == @today ?  happy_birthday : count
   end
 
-  def happy_birthday(name)
-    return "Happy Birthday, #{name}!"
+  def happy_birthday
+    return "Happy Birthday, #{@name}!"
   end 
 
-  def count(name)
+  def count
     if @birthday.to_i > @today.to_i
-    "#{name}, your birthday is in #{@birthday.to_i - @today.to_i} days"
+    "#{@name}, your birthday is in #{@birthday.to_i - @today.to_i} days"
     else
-    "#{name}, your birthday is in #{365 - (@today.to_i - @birthday.to_i)} days"
+    "#{@name}, your birthday is in #{365 - (@today.to_i - @birthday.to_i)} days"
     end
   end 
 end 
